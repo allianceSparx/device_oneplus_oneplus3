0222da40929a1c2f1fce626dae227208ff142757
martinusbe <martinusbe@gmail.com>
2018-06-03 18:32:22 +0200

    op3: build Snap

--- device.mk
+++ device.mk
@@ -201,7 +201,8 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service.oneplus3 \
     vendor.qti.hardware.camera.device@1.0 \
-    vendor.qti.hardware.camera.device@1.0_vendor
+    vendor.qti.hardware.camera.device@1.0_vendor \
+    Snap
 
 # Connectivity Engine support (CNE)
 PRODUCT_PACKAGES += \
