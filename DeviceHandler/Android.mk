781bea5c39e47c2c00f60e4f00a5bf29974e1a97
alliancesparx <alliancesparx@gmail.com>
2020-04-23 10:02:32 +0100

    Op3: Final devicehandler fix

--- DeviceHandler/Android.mk
+++ DeviceHandler/Android.mk
@@ -19,9 +19,11 @@ include $(CLEAR_VARS)
 
 LOCAL_SRC_FILES := $(call all-java-files-under, src)
 LOCAL_CERTIFICATE := platform
+LOCAL_PRIVILEGED_MODULE := true
 LOCAL_PACKAGE_NAME := DeviceHandler
 LOCAL_PRIVATE_PLATFORM_APIS := true
 LOCAL_PROGUARD_FLAG_FILES := proguard.flags
 LOCAL_DEX_PREOPT := false
+LOCAL_MODULE_TAGS := optional
 
 include $(BUILD_PACKAGE)
